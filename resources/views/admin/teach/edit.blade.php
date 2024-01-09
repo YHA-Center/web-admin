03207c01b7e7d43a9604f00f4c132d6c350bda89
Hein-HtetSan <heinhtetsan33455@gmail.com>
2024-01-09 18:52:05 +0630

    fix update teach table error

--- resources/views/admin/teach/edit.blade.php
+++ resources/views/admin/teach/edit.blade.php
@@ -31,26 +31,31 @@
 
                                 {{-- Subject  --}}
                                 <div class="mb-3 form-group">
-                                    <input type="hidden" name="" class="subject_count" value="{{ count($subjects) }}">
+                                    <input type="hidden" name="" class="subject_count"
+                                        value="{{ count($subjects) }}">
                                     <label for="name" class="form-label h6 my-2">Subject</label>
                                     <div class="row subject-group">
                                         {{-- Subject Groups  --}}
-                                        <input type="hidden" id="sub_count" name="" value="{{ count($data->subjects) }}">
+                                        <input type="hidden" id="sub_count" name=""
+                                            value="{{ count($data->subjects) }}">
                                         @foreach ($data->subjects as $item)
-                                        <div class="col-6 mb-2">
-                                            <div class="input-group input-group-merge">
-                                                <span class="input-group-text"><i class="bx bx-file"></i></span>
-                                                <select class="form-select " name="subjects[{{ $item->id }}][id]" aria-label="Default select example">
-                                                    <option selected>Choose Subject</option>
-                                                    @foreach ($subjects as $subject)
-                                                        <option value="{{ $subject->id }}" @if ($subject->id == $item->id)
-                                                            selected
-                                                        @endif>{{ $subject->name }}</option>
-                                                    @endforeach
-                                                </select>
-                                                <button class="btn btn-outline-danger btn-sm subject_del" type="button"><i class="bx bx-trash"></i> </button>
+                                            <div class="col-6 mb-2">
+                                                <div class="input-group input-group-merge">
+                                                    <span class="input-group-text"><i class="bx bx-file"></i></span>
+                                                    <select class="form-select " name="subjects[{{ $item->id }}][id]"
+                                                        aria-label="Default select example">
+                                                        <option selected>Choose Subject</option>
+                                                        @foreach ($subjects as $subject)
+                                                            <option value="{{ $subject->id }}"
+                                                                @if ($subject->id == $item->id) selected @endif>
+                                                                {{ $subject->name }}</option>
+                                                        @endforeach
+                                                    </select>
+                                                    <button class="btn btn-outline-danger btn-sm subject_del_other"
+                                                        data-item-id="{{ $item->id }}" type="button"><i
+                                                            class="bx bx-trash"></i> </button>
+                                                </div>
                                             </div>
-                                        </div>
                                         @endforeach
                                     </div>
                                     <div class="row my-2">
@@ -60,7 +65,7 @@
                                             </button>
                                         </div>
                                     </div>
-                                    
+
                                     @error('name')
                                         <div class="invalid-feedback">
                                             {{ $message }}
@@ -79,29 +84,44 @@
         </div>
     </main>
 
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
+        $(document).ready(function() {
+            $('.subject_del_other').on('click', function() {
+                // Find the parent element with class 'col-6'
+                var col6Element = $(this).closest('.col-6');
 
-document.addEventListener('DOMContentLoaded', function () {
-    const container = document.querySelector('.subject-group');
-    const add_subject_btn = document.querySelector(".subject_btn");
-    let subject_count = document.querySelector('.subject_count').value;
-    let subject = document.querySelector('#sub_count').value;  
-    
-    // control the subject add button
-    let control_subject = (ele) => {
-        if(ele > subject_count-1){
-            add_subject_btn.classList.add('d-none');
-        }else{
-            add_subject_btn.classList.remove('d-none');
-        }
-    }
+                // Do something with the 'col-6' element
+                col6Element.remove();
+            });
+        });
+    </script>
 
-    // for subject
-    add_subject_btn.addEventListener('click', () => {
-        subject++;   // increment the start
-        const newSubject = document.createElement('div');
-        newSubject.classList.add('col-6', 'mb-2');
-        newSubject.innerHTML = `
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const container = document.querySelector('.subject-group');
+            const add_subject_btn = document.querySelector(".subject_btn");
+            let subject_count = document.querySelector('.subject_count').value;
+            let subject = document.querySelector('#sub_count').value;
+
+            // control the subject add button
+            let control_subject = (ele) => {
+                if (ele > subject_count - 1) {
+                    add_subject_btn.classList.add('d-none');
+                } else {
+                    add_subject_btn.classList.remove('d-none');
+                }
+            }
+
+
+
+            // for subject
+            add_subject_btn.addEventListener('click', () => {
+                subject++; // increment the start
+                const newSubject = document.createElement('div');
+                newSubject.classList.add('col-6', 'mb-2');
+                newSubject.innerHTML = `
             {{-- Subjects --}}
             <div class="input-group input-group-merge">
                 <span class="input-group-text"><i class="bx bx-file"></i></span>
@@ -114,24 +134,22 @@ document.addEventListener('DOMContentLoaded', function () {
                 <button class="btn btn-outline-danger btn-sm subject_del" type="button"><i class="bx bx-trash"></i> </button>
             </div>
         `;
-        container.appendChild(newSubject);
+                container.appendChild(newSubject);
 
-        // Update the event listener for delete button
-        const del_subject_btn = newSubject.querySelector(".subject_del");
-        del_subject_btn.addEventListener('click', () => {
-            // Handle delete button click here
-            container.removeChild(newSubject);
-            subject -= 1;
-            // control the subject button 
-            control_subject(subject);
+                // Update the event listener for delete button
+                const del_subject_btn = newSubject.querySelector(".subject_del");
+                del_subject_btn.addEventListener('click', () => {
+                    // Handle delete button click here
+                    container.removeChild(newSubject);
+                    subject -= 1;
+                    // control the subject button 
+                    control_subject(subject);
+                });
+
+                // control the subject button 
+                control_subject(subject)
+            });
+
         });
-
-        // control the subject button 
-        control_subject(subject)
-    });
-
-});
-
-
     </script>
 @endsection
